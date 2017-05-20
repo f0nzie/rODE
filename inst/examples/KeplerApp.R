5eb0aafb0229d07d2ec0af8b2dd24f21ec754c36
alfonso zbook <zbook@reyesaguayo.com>
2017-05-19 21:19:23 -0500

    add printing to KeplerApp. create data folder.

--- inst/examples/KeplerApp.R
+++ inst/examples/KeplerApp.R
@@ -1,7 +1,7 @@
-#' 
+#'
 #' KeplerApp.R
-#' 
-#' 
+#'
+#'
 source("./R/ode_generics.R")
 source("./R/RK45.R")
 source("./inst/examples/Kepler.R")
@@ -19,7 +19,7 @@ solver <- RK45(planet)
 while (planet@state[5] <= 10) {
     solver <- step(solver)
     planet <- solver@ode
-    cat(sprintf("state[1]=%12f, state[2]= %12f,  state[3]=%12f, state[5]=%12f\n", 
+    cat(sprintf("state[1]=%10f, state[2]= %10f,  state[3]=%10f, state[5]=%10f\n",
                 planet@state[1],
                 planet@state[2], planet@state[3], planet@state[5]))
 }
@@ -27,5 +27,5 @@ while (planet@state[5] <= 10) {
 # at t=100, dt=0.1,  c(2.131958,     1.105316,   100.000000)
 # Java: state[0] = 0.444912, state[1]= -1.436203, state[2]= 0.459081, state[4]= 10.033245
 #       currentStep=    0.061646
-# R:    state[1] = 0.444912, state[2]= -1.436203, state[3]= 0.459081, state[5]= 10.033245 
+# R:    state[1] = 0.444912, state[2]= -1.436203, state[3]= 0.459081, state[5]= 10.033245
 #       currentStep= 0.06164632
