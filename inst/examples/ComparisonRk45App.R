fad315e0e68bbe1bd61de098b4dac6b8991ef2e2
alfonso zbook <zbook@reyesaguayo.com>
2017-05-23 19:56:31 -0500

    clean up

--- inst/examples/ComparisonRk45App.R
+++ inst/examples/ComparisonRk45App.R
@@ -15,16 +15,11 @@ ode_solver <- setTolerance(ode_solver, 1e-8)
 
 time <-  0
 
-
 while (time < 50) {
     ode_solver <- step(ode_solver)
     stepSize <-  ode_solver@stepSize     # update the step size
     time <- time + stepSize
-    # ode <- ode_solver@ode
     state <- getState(ode_solver@ode)
-    # cat("time=", time, "\t xl=", state[1],
-    #     "\t error=", (state[1] - getExactSolution(ode_solver@ode, time)),
-    #     ode_solver@ode@n, "\n")
     cat(sprintf("time=%10f xl=%14e error=%14e n=%5d \n", time, state[1],
                 (state[1] - getExactSolution(ode_solver@ode, time)),
                 getRateCounts(ode)))
