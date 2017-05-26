65c530a26a737763abd05296e89829beaf5fa304
alfonso zbook <zbook@reyesaguayo.com>
2017-05-26 15:36:19 -0500

    use class in examples

--- inst/examples/KeplerDormandPrince45App.R
+++ inst/examples/KeplerDormandPrince45App.R
@@ -5,56 +5,53 @@
 #
 #
 
-source(paste(system.file("examples", package = "rODE"),
-             "KeplerDormandPrince45.R",
-             sep ="/"))
-
+importFromExamples("KeplerDormandPrince45.R")
 
 KeplerDormandPrince45App <- function(verbose = FALSE) {
-
-    particle <- Kepler()
-
+    # values for the examples
     x  <- 1
     vx <- 0
     y  <- 0
     vy <- 2 * pi
-    dt <- 0.01
-    tol <- 1e-3
+    dt <- 0.01          # step size
+    tol <- 1e-3         # tolerance
 
+    particle <- Kepler()
     particle <- init(particle, c(x, vx, y, vy, 0))
 
-    odeSolver <- DormandPrince45(particle)
-    odeSolver <- init(odeSolver, dt)
+    odeSolver <- DormandPrince45(particle)      # select the ODE solver
+    odeSolver <- init(odeSolver, dt)            # start the solver
 
     odeSolver <- setTolerance(odeSolver, tol)   # this works for adaptive solvers
+    particle@odeSolver <- odeSolver             # copy the solver to ODE object
+    initialEnergy <- getEnergy(particle)        # calculate the energy
 
-    particle@odeSolver <- odeSolver
-
-    initialEnergy <- getEnergy(particle)
-    i <- 0
+    rowVector <- vector("list")
+    i <- 1
     while (getTime(particle) < 1.5) {
-        # odeSolver <- step(odeSolver)
-        particle <- doStep(particle)
-        # odeSolver <- particle@odeSolver
-        energy <- getEnergy(particle)
+        rowVector[[i]] <- list(t = particle@state[5], s1 = particle@state[1],
+                               s2 = particle@state[2], s3 = particle@state[3],
+                               s4 = particle@state[4],
+                               energy = getEnergy(particle) )
+        particle <- doStep(particle)            # advance one step
+        energy   <- getEnergy(particle)         # calculate energy
 
         if (verbose)
-        cat(sprintf("time=%12f energy=%12f state[5]=%12f x=%10f y=%10f \n",
+            cat(sprintf("time=%12f energy=%12f state[5]=%12f x=%10f y=%10f \n",
                     getTime(particle),
                     energy, particle@state[5], particle@state[1], particle@state[3]))
         i <- i + 1
     }
-    return(list(getTime(particle), energy, particle@state[5], particle@state[1],
-                particle@state[3]
-                ))
-
+    DT <- data.table::rbindlist(rowVector)
+    return(DT)
 }
 
 
-KeplerDormandPrince45App()
+DT <- KeplerDormandPrince45App()
+plot(DT)
 
 
-# Values from Java at t approx = 1.5
+# Values from Java simulator at t approx = 1.5
 # time=    1.444831 energy=  -19.737930 state[4]=    1.444831
 # time=    1.507215 energy=  -19.737875 state[4]=    1.507215
 # time=    1.569599 energy=  -19.737819 state[4]=    1.569599
