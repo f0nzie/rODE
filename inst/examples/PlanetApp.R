9ff2d55738f06bc5f4938bd505e091a6b18506dc
alfonso zbook <zbook@reyesaguayo.com>
2017-05-27 12:26:24 -0500

    reduce from 365 to 90 days orbit simulation in PlanetApp.R. bump version to 0.99.4

--- inst/examples/PlanetApp.R
+++ inst/examples/PlanetApp.R
@@ -15,9 +15,9 @@ PlanetApp <- function(verbose = FALSE) {
     rowvec <- vector("list")
     i <- 1
     # run infinite loop. stop with ESCAPE.
-    while (planet@state[5] <= 365) {     # Earth orbit is 365 days around the sun
-        rowvec[[i]] <- list(t  = planet@state[5],
-                            x  = planet@state[1],
+    while (planet@state[5] <= 90) {     # Earth orbit is 365 days around the sun
+        rowvec[[i]] <- list(t  = planet@state[5],     # just doing 3 months
+                            x  = planet@state[1],     # to speed up for CRAN
                             vx = planet@state[2],
                             y  = planet@state[3],
                             vy = planet@state[4])
@@ -31,7 +31,7 @@ PlanetApp <- function(verbose = FALSE) {
 }
 # run the application
 solution <- PlanetApp()
-select_rows <- seq(1, 7301, 20)      # do not overplot
+select_rows <- seq(1, nrow(solution), 10)      # do not overplot
 solution <- solution[select_rows,]
 plot(solution)
 
