f4dcf8b7d7390af32be896a9c247d05aec28386d
f0nzie zbook <f0nzie@oilgainsanalytics.com>
2017-11-01 17:18:39 -0500

    move generic to top. Move examples from generic to method

--- R/AbstractODESolver.R
+++ R/AbstractODESolver.R
@@ -25,6 +25,12 @@
                     contains = c("ODESolver")
                     )
 
+#' AbstractODESolver generic
+#'
+#' @rdname AbstractODESolver-class
+#' @export
+setGeneric("AbstractODESolver", function(ode, ...)  standardGeneric("AbstractODESolver"))
+
 
 setMethod("initialize", "AbstractODESolver", function(.Object, .ode, ...) {
     .Object <- init(.Object, 0.1)                   # default value for stepSize
@@ -83,37 +89,6 @@ setMethod("getStepSize", "AbstractODESolver", function(object, ...) {
     return(object@stepSize)
 })
 
-#' AbstractODESolver
-#'
-#' @rdname AbstractODESolver-class
-#' @export
-#' @examples
-#' # This is how we start defining a new ODE solver: Euler
-#' .Euler <- setClass("Euler",              # Euler solver very simple; no slots
-#'      contains = c("AbstractODESolver"))
-#'
-#'
-#'
-#' # Here we define the ODE solver Verlet
-#' .Verlet <- setClass("Verlet", slots = c(
-#'     rate1 = "numeric",                          # Verlet calculates two rates
-#'     rate2 = "numeric",
-#'     rateCounter = "numeric"),
-#' contains = c("AbstractODESolver"))
-#'
-#'
-#'
-#' # This is the definition of the ODE solver Runge-Kutta 4
-#' .RK4 <- setClass("RK4", slots = c(       # On the other hand RK4 uses 4 rates
-#'    rate1 = "numeric",
-#'    rate2 = "numeric",
-#'    rate3 = "numeric",
-#'    rate4 = "numeric",
-#'    estimated_state = "numeric"),         # and estimates another state
-#' contains = c("AbstractODESolver"))
-#'
-setGeneric("AbstractODESolver", function(ode, ...)
-    standardGeneric("AbstractODESolver"))
 
 
 #' AbstractODESolver
@@ -144,6 +119,31 @@ setMethod("AbstractODESolver", signature(ode = "missing"), function(ode, ...) {
 #'
 #' @importFrom methods new
 #' @export
+#' @examples
+#' # This is how we start defining a new ODE solver: Euler
+#' .Euler <- setClass("Euler",              # Euler solver very simple; no slots
+#'      contains = c("AbstractODESolver"))
+#'
+#'
+#'
+#' # Here we define the ODE solver Verlet
+#' .Verlet <- setClass("Verlet", slots = c(
+#'     rate1 = "numeric",                          # Verlet calculates two rates
+#'     rate2 = "numeric",
+#'     rateCounter = "numeric"),
+#' contains = c("AbstractODESolver"))
+#'
+#'
+#'
+#' # This is the definition of the ODE solver Runge-Kutta 4
+#' .RK4 <- setClass("RK4", slots = c(       # On the other hand RK4 uses 4 rates
+#'    rate1 = "numeric",
+#'    rate2 = "numeric",
+#'    rate3 = "numeric",
+#'    rate4 = "numeric",
+#'    estimated_state = "numeric"),         # and estimates another state
+#' contains = c("AbstractODESolver"))
+#'
 setMethod("AbstractODESolver", signature(ode = "ODE"), function(ode, ...) {
     odesolver <- .AbstractODESolver(ode = ode)
     odesolver@ode <- ode
